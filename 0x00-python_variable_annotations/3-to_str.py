514313e40513d56031792fea58767933581ef565
root <ejimovc@gmail.com>
2024-08-01 17:48:43 +0000

    Task 3 and 9 corrected

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -3,4 +3,6 @@
 
 
 def to_str(n: float) -> str:
+    """A function that takes
+    a foat and return the str rep of the float"""
     return n.__str__()
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Python file"""
-from typing import List, Tuple, Sequence
+from typing import List, Iterable, Tuple, Sequence
 
 
-def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """A function to annotate"""
     return [(i, len(i)) for i in lst]
